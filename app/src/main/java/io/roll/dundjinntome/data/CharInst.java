5c33f023063bdbf5f2e2a88f5ecde4e606d5fc47
Daniel Larsen <danny.r.larsen@gmail.com>
2018-03-27 14:35:30 -0500

    Refactoring

---
+++ app/src/main/java/io/roll/dundjinntome/data/CharInst.java
@@ -0,0 +1,12 @@
+package io.roll.dundjinntome.data;
+
+public class CharInst {
+private static final CharInst ourInstance=new CharInst();
+
+public static CharInst getInstance(){
+        return ourInstance;
+        }
+
+private CharInst(){
+        }
+        }
---
+++ app/src/main/java/io/roll/dundjinntome/viewCharacter/ViewCharacterContract.java
@@ -0,0 +1,17 @@
+package io.roll.dundjinntome.viewCharacter;
+
+import io.roll.dundjinntome.BasePresenter;
+import io.roll.dundjinntome.BaseView;
+import io.roll.dundjinntome.data.CharInst;
+
+public interface ViewCharacterContract {
+    //TODO Code Interface Contract
+    interface View extends BaseView<Presenter>{
+
+    }
+
+    interface Presenter extends BasePresenter{
+
+    }
+
+}
